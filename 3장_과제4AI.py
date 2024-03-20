2803538c2ea3a8882092dcf868ce818fd44cc454
dzashaa <dzikris.amaliah@gmail.com>
2024-03-20 22:38:48 +0900

    test 5

---
+++ 3장_과제4AI.py
@@ -0,0 +1,221 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/김아메/AI/과제/conditional_evaluations.py ===============
+1. False
+2. False
+3. True
+4. True
+5. False
+6. True
+7. True
+8. False
+9. True
+10. True
+11. False
+12. True
+13. True
+14. True
+15. True
+16. False
+
+==================== RESTART: C:/김아메/AI/과제/amusement_ride.py ===================
+나이를 입력하시오: 23
+키를 입력하시오(단위 cm): 160
+입징할 수 있습니다.
+
+==================== RESTART: C:/김아메/AI/과제/amusement_ride.py ===================
+나이를 입력하시오: 16
+키를 입력하시오(단위 cm): 162
+입장 할 수 없습니다.
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/integer_sort.py
+두 정수를 입력하시오: 99 12
+12 99
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/game_score_checker.py
+게임점수를 입력하시오: 990
+Traceback (most recent call last):
+  File "C:/Users/진한섭/AppData/Local/Programs/Python/Python312/game_score_checker.py", line 10, in <module>
+    main()
+  File "C:/Users/진한섭/AppData/Local/Programs/Python/Python312/game_score_checker.py", line 4, in main
+    if game_sore >= 1000:
+NameError: name 'game_sore' is not defined. Did you mean: 'game_score'?
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/game_score_checker.py
+게임점수를 입력하시오: 990
+입문자입니다.
+
+================= RESTART: C:/김아메/AI/과제/divisibility_checker.py ================
+정수를 입력하시오: 15
+15는 (은) 3(으)로 나누어집니다.
+
+================= RESTART: C:/김아메/AI/과제/divisibility_checker.py ================
+정수를 입력하시오: 12
+12는 (은) 2와(과) 3 모두로 나누어지지 않습니다.
+
+================= RESTART: C:/김아메/AI/과제/divisibility_checker.py ================
+정수를 입력하시오: 15
+15는 (은) 3(으)로 나누어집니다.
+
+==================== RESTART: C:/김아메/AI/과제/lottery_system.py ===================
+세 복권번호를 입력하시오: 1 5 7
+다음 기회에...
+
+==================== RESTART: C:/김아메/AI/과제/lottery_system.py ===================
+세 복권번호를 입력하시오: 1 2 7
+상금 1만 원
+
+==================== RESTART: C:/김아메/AI/과제/lottery_system.py ===================
+세 복권번호를 입력하시오: 1 2 9
+상금 1천만 원
+
+=================== RESTART: C:/김아메/AI/과제/point_in_circle.py ===================
+점의 좌표 x, y를 입력하시오: -9 -5
+원의 외부에 있음
+
+=================== RESTART: C:/김아메/AI/과제/point_in_circle.py ===================
+점의 좌표 x, y를 입력하시오: 7 9
+원의 내부에 있음
+
+=============== RESTART: C:/김아메/AI/과제/multification_table_for.py ===============
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+2 * 6 = 12
+2 * 7 = 14
+2 * 8 = 16
+2 * 9 = 18
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/multification_table_while.py
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+2 * 6 = 12
+2 * 7 = 14
+2 * 8 = 16
+2 * 9 = 18
+
+================== RESTART: C:/김아메/AI/과제/program1_for_loop.py ==================
+ Python 
+is 
+FUN! 
+ Python 
+is 
+FUN! 
+ Python 
+is 
+FUN! 
+
+================== RESTART: C:/김아메/AI/과제/program2_for_loop.py ==================
+Python 
+is.
+FUNI 
+Python 
+is.
+FUNI 
+Python 
+is.
+FUNI 
+
+================== RESTART: C:/김아메/AI/과제/program2_for_loop.py ==================
+Python 
+is.
+FUN! 
+Python 
+is.
+FUN! 
+Python 
+is.
+FUN! 
+
+================== RESTART: C:/김아메/AI/과제/program3_for_loop.py ==================
+Python 
+is.
+FUNI 
+Python 
+is.
+FUNI 
+Python 
+is.
+FUNI 
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/menu_ordering.py
+맛나 식당에 오신 것을 환영합니다. 메뉴는 다음과 같습니다.
+- 햄버거(입력 b)
+- 치킨(입력 c)
+- 피자(입력 p)
+메뉴를 선택하세요(알파벳 b, c, p 입력): c
+치킨을 선택하였습니다.
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/menu_ordering.py
+맛나 식당에 오신 것을 환영합니다. 메뉴는 다음과 같습니다.
+- 햄버거(입력 b)
+- 치킨(입력 c)
+- 피자(입력 p)
+메뉴를 선택하세요(알파벳 b, c, p 입력): X
+메뉴를 다시 입력하세요(알파벳 b, c, p 입력): 
+메뉴를 선택하세요(알파벳 b, c, p 입력): c
+치킨을 선택하였습니다.
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/prime_checker.py
+숫자를 입력하세요: 5
+5는 소수입니다.
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/prime_checker.py
+숫자를 입력하세요: 9
+9는 소수가 아닙니다.
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/sum_of_square.py
+숫자를 입력하세요: 5
+결과는 55입니다.
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/snail_escape.py
+day : 1 달팽이의 위치 : 2 미터
+day : 2 달팽이의 위치 : 4 미터
+day : 3 달팽이의 위치 : 6 미터
+day : 4 달팽이의 위치 : 8 미터
+day : 5 달팽이의 위치 : 10 미터
+day : 6 달팽이의 위치 : 12 미터
+day : 7 달팽이의 위치 : 14 미터
+day : 8 달팽이의 위치 : 16 미터
+day : 9 달팽이의 위치 : 18 미터
+day : 10 달팽이의 위치 : 20 미터
+day : 11 달팽이의 위치 : 22 미터
+day : 12 달팽이의 위치 : 24 미터
+
+우물을 탈출하는 데 걸린 날은 13일 입니다.
+>>> 
+================== RESTART: C:/김아메/AI/과제/armstrong_numbers.py ==================
+세 자리의 암스트롱 수: 153 370 371 407
+>>> 
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/fuel_tank_simulation.py
+충전 또는 사용한 연료를 +/- 기호와 함께 입력하시오: +60
+현재 탱크양은 560 입니다.
+충전 또는 사용한 연료를 +/- 기호와 함께 입력하시오: -300
+현재 탱크양은 260 입니다.
+충전 또는 사용한 연료를 +/- 기호와 함께 입력하시오: -220
+현재 탱크양은 40 입니다.
+경고: 연료가 10% 미만이니 충전하세요!
+>>> 
+=================== RESTART: C:/김아메/AI/과제/amicable_numbers.py ==================
+220의 친화수 284
+284의 친화수 220
+1184의 친화수 1210
+1210의 친화수 1184
+2620의 친화수 2924
+2924의 친화수 2620
+5020의 친화수 5564
+5564의 친화수 5020
+6232의 친화수 6368
+6368의 친화수 6232
+10744의 친화수 10856
+10856의 친화수 10744
+12285의 친화수 14595
+14595의 친화수 12285
+17296의 친화수 18416
+18416의 친화수 17296
